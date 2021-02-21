8c796dc7c6a86e2fa41a9846dd6a56e4e81be2d5
nandini-garg4 <73828366+nandini-garg4@users.noreply.github.com>
2021-02-21 14:10:09 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,12 @@
+class Food{
+    constructor(){
+var foodStock
+    
+}
+
+
+this.image=loadImage("Milk.png")
+this.body=Bodies.circle(this.x,this.y,(this.r)/2,options)
+
+World.add(world, this.body);
+}
